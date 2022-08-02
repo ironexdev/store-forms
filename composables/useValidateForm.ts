d89790aa7877c1789526aaf19090ae4ff35f069d
Ironex <patrik.ht01@gmail.com>
2022-08-02 12:30:29 +0200

    Fixed: checkbox support

--- composables/useValidateForm.ts
+++ composables/useValidateForm.ts
@@ -1,48 +1,45 @@
 import { useResetForm } from "./useResetForm";
 import FieldInterface from "../Form/Field/FieldInterface";
 
-export function useValidateForm(fields: FieldInterface[], valid: boolean): {[key: string]: string[]} {
-    const errors = {}
+export function useValidateForm (fields: FieldInterface[], valid: boolean): {[key: string]: string[]} {
+  const errors = {}
 
-    useResetForm(fields)
+  useResetForm(fields)
 
-    for (let i = 0; i < fields.length; i++) {
-      const field: FieldInterface = fields[i]
-      const required = field.required
+  for (let i = 0; i < fields.length; i++) {
+    const field: FieldInterface = fields[i]
+    const required = field.required
 
-      if (required && typeof field.value === "boolean" ? !field.value : !field.value.length) {
-        valid = false
+    if (required && (typeof field.value === "boolean" ? !field.value : !field.value.length)) {
+      field.errors.push(field.requiredErrorMessage)
 
-        field.errors.push(field.requiredErrorMessage)
-
-        if (typeof errors[field.name] === "undefined") {
-          errors[field.name] = []
-        }
-
-        errors[field.name].push(field.requiredErrorMessage)
-
-        continue
+      if (typeof errors[field.name] === "undefined") {
+        errors[field.name] = []
       }
 
-      if (required || (!required && field.value.length)) {
-        for (let ii = 0; ii < field.rules.length; ii++) { // Validate rules
-          const rule = field.rules[ii]
+      errors[field.name].push(field.requiredErrorMessage)
 
-          if (!rule.test(field.value)) {
-            valid = false
+      continue
+    }
 
-            const message = rule.message(field.title, field.value)
-            field.errors.push(message)
+    if (required || (!required && field.value.length)) {
+      for (let ii = 0; ii < field.rules.length; ii++) { // Validate rules
+        const rule = field.rules[ii]
 
-            if (typeof errors[field.name] === "undefined") {
-              errors[field.name] = []
-            }
+        if (!rule.test(field.value)) {
+          const message = rule.message(field.title, field.value)
+          field.errors.push(message)
 
-            errors[field.name].push(message)
+          if (typeof errors[field.name] === "undefined") {
+            errors[field.name] = []
           }
+
+          errors[field.name].push(message)
         }
       }
     }
+  }
 
-    return errors
+  return errors
 }
+
